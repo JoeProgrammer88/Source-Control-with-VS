003c34f27df737fba37a3fc3be519e29b5d4f4bd
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2022-04-19 10:45:04 -0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Please enter the day of the week");
+
+string dayOfWeek = Console.ReadLine();
+dayOfWeek = dayOfWeek.ToLower();
+
+switch (dayOfWeek)
+{
+    case "monday":
+        Console.WriteLine("Monday is cereal");
+        break;
+    case "tuesday":
+        Console.WriteLine("Tuesday is donuts");
+        break;
+    case "wednesday":
+        Console.WriteLine("Wednesday is eggs");
+        break;
+}
+
+if (dayOfWeek == "monday")
+{
+
+}
+else if (dayOfWeek == "tuesday")
+{
+
+}
+else if (dayOfWeek == "wednesday")
+{
+
+}
